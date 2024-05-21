1fd9ad2409c339f8c4f1fe843484f99e01f50562
josephryu7656 <103790345+josephryu7656@users.noreply.github.com>
2024-05-22 09:33:50 +1200

    Update fence.py
    Added a valueerror and better formating of the code

--- fence.py
+++ fence.py
@@ -4,23 +4,25 @@
 
 
 while True:
-    width = int(input("Width > "))
-    length = int(input("Length > "))
-    m_cost = int(input("Cost/m > "))
+    print("---------")
+    try:
+        width = int(input("Width > "))
+        length = int(input("Length > "))
+        m_cost = int(input("Cost/m > "))
 
-    if width > 0 and length > 0:
-        perimeter = width + length
-        perimeter += perimeter
-        cost = perimeter*m_cost
-        print(f"Perimeter: {perimeter} m")
-        print(f"Cost: ${cost}")
-        pass
-    else: 
-        print("Enter a valid input")
-    again = float(input("Again?"))
-    if again >= 0:
-        print("<loops>")
-    else:
-        break
+        if width > 0 and length > 0:
+            perimeter = width + length
+            perimeter += perimeter
+            cost = perimeter*m_cost
+            print(f"Perimeter: {perimeter} m")
+            print(f"Cost: ${cost}")
+
+            input("Again <enter> >")
+            pass
+        else:
+            print("Please enter a valid value")
+            break
+    except ValueError:
+        print("Error")
 
     
